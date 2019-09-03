2381452b913077c6bddad4bd23c53427bc72ee5e
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2019-09-03 15:15:44 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,38 @@
+def using_push (array, string)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+   colors_in_the_rainbow.push("violet")
+end
+
+def using_unshift (array, string)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  bouroughs_in_nyc.unshift ("Staten Island")
+end
+
+def using_pop (array)
+ continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+continents = array.pop
+ 
+end
+
+def pop_with_args (array)
+  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  dog_breeds = array.pop(2)
+end
+
+def using_shift(array)
+  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+  array.shift
+end
+def shift_with_args(array)
+  ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+  array.shift(2)
+end
+
+def using_concat (my_favorite_things, more_favs)
+  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+    more_favs = ["sports cars", "flatiron school"]
+  my_favorite_things.concat(more_favs)
+  end
+
+def using_insert(list_of_programming_languages,another)
+  list_of_programming_languages = [""]
