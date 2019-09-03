02e01a4ce6c395e2cfe386ba5efe0b2aa84d8b57
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2019-09-03 16:21:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -48,7 +48,7 @@ def using_flatten (array)
   array.flatten
 end
 
-def using_delete (array, String)
+def using_delete (array, string)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
   array.delete ("Steven")
 end
