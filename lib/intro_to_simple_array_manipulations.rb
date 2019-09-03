188262e0ded24ffca0a5e535d4bcafe7e25a1c02
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2019-09-03 15:20:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,14 +29,11 @@ def shift_with_args(array)
 end
 
 def using_concat (my_favorite_things, more_favs)
-  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-    more_favs = ["sports cars", "flatiron school"]
+ 
   my_favorite_things.concat(more_favs)
   end
 
 def using_insert(list_of_programming_languages,another_language)
-  list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
-  another_language = ["Python"]
   list_of_programming_languages.insert[4]
 end
   
